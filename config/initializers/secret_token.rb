64172fe5fbd7f186fb63702b0b490b0a09041529
Loren Hale <loren.hale@gmail.com>
2012-03-27 19:25:16 +0800

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Nova::Application.config.secret_token = '897b79b01d13eb030da0250ddf34ec3a29939a200cf33422889de8eb3b78c78c92198e52daaa8dd30d8e583e85f14773cf6147cb9ff3894887af061bf62ca492'
