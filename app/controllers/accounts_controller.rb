8570537281df302777ecefcacd8168d309ffdd8d
Loren Hale <loren.hale@gmail.com>
2012-03-27 20:05:55 +0800

    modified controller

--- app/controllers/accounts_controller.rb
+++ app/controllers/accounts_controller.rb
@@ -1,30 +1,18 @@
 class AccountsController < ApplicationController
+
   # GET /accounts
-  # GET /accounts.json
   def index
     @accounts = Account.all(:limit => 10)
   end
 
   # GET /accounts/1
-  # GET /accounts/1.json
   def show
     @account = Account.find(params[:id])
-
-    respond_to do |format|
-      format.html # show.html.erb
-      format.json { render json: @account }
-    end
   end
 
   # GET /accounts/new
-  # GET /accounts/new.json
   def new
     @account = Account.new
-
-    respond_to do |format|
-      format.html # new.html.erb
-      format.json { render json: @account }
-    end
   end
 
   # GET /accounts/1/edit
@@ -33,46 +21,35 @@ class AccountsController < ApplicationController
   end
 
   # POST /accounts
-  # POST /accounts.json
   def create
     @account = Account.new(params[:account])
 
-    respond_to do |format|
       if @account.save
-        format.html { redirect_to @account, notice: 'Account was successfully created.' }
-        format.json { render json: @account, status: :created, location: @account }
+        redirect_to @account, notice: 'Account was successfully created.'
       else
-        format.html { render action: "new" }
-        format.json { render json: @account.errors, status: :unprocessable_entity }
+        render action: "new"
       end
-    end
+
   end
 
   # PUT /accounts/1
-  # PUT /accounts/1.json
   def update
     @account = Account.find(params[:id])
 
-    respond_to do |format|
       if @account.update_attributes(params[:account])
-        format.html { redirect_to @account, notice: 'Account was successfully updated.' }
-        format.json { head :no_content }
+        redirect_to @account, notice: 'Account was successfully updated.'
       else
-        format.html { render action: "edit" }
-        format.json { render json: @account.errors, status: :unprocessable_entity }
+        render action: "edit"
       end
-    end
+
   end
 
   # DELETE /accounts/1
-  # DELETE /accounts/1.json
   def destroy
     @account = Account.find(params[:id])
     @account.destroy
 
-    respond_to do |format|
-      format.html { redirect_to accounts_url }
-      format.json { head :no_content }
-    end
+    redirect_to accounts_url
   end
+  
 end
